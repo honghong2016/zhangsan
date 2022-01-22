89fc08cc7a4da4cc82e0b1d9d60930509dee6b5b
honghong2016 <1137721308@qq.com>
2022-01-22 21:20:22 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,22 @@
+# """
+# print("你好")  #字符串
+# print(2333)    #整数
+# print(2.333)
+# print(True)
+# print(())
+# print([])
+# print({})
+# # """
+# # 锄禾日当午
+# # 汗滴禾下土
+# # """
+# print("呵呵",2333)
+# r="34" 
+# print(r)
+# """
+# a = float(input("请输入:"))
+# b = float(input("请输入:"))
+# print("input获取的内容:",a+b)
+a = "我们"
+b = "我马上"
+print(len(a)+len(b))
